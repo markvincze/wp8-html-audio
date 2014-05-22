1d3f899e8af375f590b5074894333343648e469a
mrkvincze <mrk.vincze@gmail.com>
2014-05-22 12:58:00 +0200

    initial commit

---
+++ src/HtmlAudioTest/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using HtmlAudioTest.Resources;
+
+namespace HtmlAudioTest
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            WebBrowser.Navigate(new Uri("Assets/Html/index.html", UriKind.Relative));
+        }
+    }
+}
